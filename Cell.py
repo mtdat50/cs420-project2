e491d93156ece3d319736de85186ae5f0158817b
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2023-12-31 16:52:47 +0700

    draw blank map

---
+++ Cell.py
@@ -0,0 +1,13 @@
+
+import pygame, random
+
+class Cell(pygame.sprite.Sprite):
+    def __init__(self, picture_path, pos_x, pos_y):
+        super().__init__()
+        # Random asset for cell
+        picture_path += str(random.randrange(0, 11)) + ".png"
+
+        self.image = pygame.image.load(picture_path).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = (pos_x, pos_y)
+   
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from functions import *
 from agent import Agent
 from map import Map
+import pygame
+from constants import *
+import sys
+from Cell import Cell
+from constants import CELL_SIZE
+import random
 
 
 def main():
@@ -8,24 +14,55 @@ def main():
     map, agent.agentLoc = input("tests/test1.txt")
 
     # agent.foundExit = True
-    while agent.isAlive and not agent.isEscaping:
-        print('===========================', agent.agentLoc)
-        agent.perceiveEnvironment(map)
-        print(agent.kb.clauses)
+    # while agent.isAlive and not agent.isEscaping:
+    #     agent.updateInfo(map)
+    #     nextRoom = []
+    #     if agent.foundExit:
+    #         nextRoom = agent.findASafeStep(map.size())
 
-        nextRoom = []
-        if agent.foundExit:
-            nextRoom = agent.findASafeStep(map.size())
-            if nextRoom == [-1, -1]:
-                nextRoom = [1, 1]
-                agent.isEscaping = True
-                print('escapse')
+    #         if nextRoom == [-1, -1]:
+    #             nextRoom = [1, 1]
+    #             agent.isEscaping = True
                 
-        # else:
-        #     nextRoom = agent.forceAStep()
+    #         if nextRoom == [1, 1]:
+    #             agent.foundExit = True
+    #     else:
+    #         nextRoom = agent.forceAStep()
 
-        agent.playPath(nextRoom)
-        agent.agentLoc = nextRoom
+    #     agent.playPath(nextRoom)
+
+    # General initializations
+    pygame.display.set_caption('Wumpus World')
+    pygame.init()
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont("freesansbold" , 18 , bold = True)
+
+    # Game screen
+    screen_width = map.size() * CELL_SIZE
+    screen_height = map.size() * CELL_SIZE
+    screen = pygame.display.set_mode((screen_width, screen_height), flags=pygame.HWSURFACE)
+
+    # Cell Group
+    cellGroup = pygame.sprite.Group()
+    for i in range(map.size()):
+        for j in range(map.size()):
+            new_cell = Cell("Graphics\\catacombs_", CELL_SIZE/2 + CELL_SIZE * j, CELL_SIZE/2 + CELL_SIZE * i)
+            cellGroup.add(new_cell)
+
+
+    
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        pygame.display.flip()
+        cellGroup.draw(screen)
+        cellGroup.update()
+
+        clock.tick(120)
 
 def updateMap(shotRoomCoord):
     pass
