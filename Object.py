9b63b38d94a69b0bae028fa411365f8f046ca83c
Cohvtk <94277200+Cohvtk@users.noreply.github.com>
2024-01-05 18:07:12 +0700

    Revert "Merge branch 'main' of https://github.com/mtdat50/cs420-project2"
    This reverts commit 880c7d34e823cdc6947f4e3db213ffb30bdb68eb, reversing
    changes made to e364c799a90dafa026e9d3799849a9771164ac6b.

--- Object.py
+++ Object.py
@@ -1,10 +1,10 @@
 import pygame, random
 
 class Cell(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y, cell_info):
+    def __init__(self, picture_path, pos_x, pos_y):
         super().__init__()
         # Random floor asset
-        picture_path = "Graphics\\catacombs_" + str(random.randrange(0, 11)) + ".png"
+        picture_path += str(random.randrange(0, 11)) + ".png"
 
         self.image = pygame.image.load(picture_path).convert_alpha()
         self.rect = self.image.get_rect()
